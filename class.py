12fb8862fe92a0360d16ef94b2e814250a713e99
victor2507 <115999899+victor2507@users.noreply.github.com>
2022-10-20 10:09:23 +0200

    Update class.py

--- class.py
+++ class.py
@@ -1,7 +1,15 @@
+from random import *
+
 class Cadena():
    
-   def __init__(self, ):
-      self. = 
+   def __init__(self,nbj ):
+      self.nbj = nbj;
+      
+      
+   def generer_cadenas(self):
+      self.cadenas={};
+      for k in range (self.nbj-1):
+         self.cadenas[k]= 
       
       
 
