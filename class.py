c2f9de9113d513937afa22a6f7a3f673daa9a3fe
Elfes300p <115999882+Elfes300p@users.noreply.github.com>
2022-10-21 09:40:54 +0200

    Update class.py

--- class.py
+++ class.py
@@ -6,7 +6,7 @@ class Cadena():
       self.nbj = nbj;
             
    def generer_cadenas(self):
-      self.cadenas={};
+      self.cadenas=[];
       cars=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9"];
       
       for k in range (self.nbj-1):
