483b33d6d9c66a3ec798f0a89e83f7fa418d0d66
victor2507 <115999899+victor2507@users.noreply.github.com>
2022-10-21 09:36:20 +0200

    Update class.py

--- class.py
+++ class.py
@@ -7,9 +7,13 @@ class Cadena():
             
    def generer_cadenas(self):
       self.cadenas={};
-      cars=["A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A",]
+      cars=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9"];
+      
       for k in range (self.nbj-1):
-         self.cadenas[k]= random.choices;
+         combi = list();
+         for i in range(4):
+            combi.append(random.choice(cars));
+         self.cadenas[k] = combi;
       
       
 
